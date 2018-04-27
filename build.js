ae0ed5bc9b16fbfd0cfcd5f9ef736e2c2c6ccd09
Gene Mountjoy <mountwe@live.com>
2018-04-27 13:16:37 -0400

    Modified to use layouts with handlebars engine.

--- build.js
+++ build.js
@@ -1,5 +1,7 @@
 var metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
+var layouts = require('metalsmith-layouts');
+var handlebars = require('handlebars');
 
 metalsmith(__dirname)
   .metadata({
@@ -11,6 +13,12 @@ metalsmith(__dirname)
   .source('./src')
   .destination('./public')
   .use(markdown())
+  .use(layouts({
+      engine: 'handlebars',
+      directory: './layouts',
+      default: 'article.html',
+      pattern: ["*/*/*html","*/*html","*html"]
+  }))
   .build(function (err) {
     if (err) {
       console.log(err);
